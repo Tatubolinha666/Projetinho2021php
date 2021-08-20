228318bfe601f30d34f9a35de261070e785279d8
Tatubolinha666 <katlyn.zatta@etec.sp.gov.br>
2021-08-19 22:13:30 -0300

    atualizado

---
+++ segundo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<?php 
+#twitter é horrivel
+#mingau de aveia é bom 
+ ?>
+<head>
+	<title>Teste 1, 2 ,3</title>
+</head>
+<body> <?php 
+/* 
+várias
+linhasss*/
+echo "Exemplo 2 comentário";
+   $data=("21 de setembro de 2015");
+ ?>
+ <p>HTML interpreta este arquivo</p>
+ Arquivo criado em PHP 
+ <?php echo $data; ?>
+</body>
+</html>
